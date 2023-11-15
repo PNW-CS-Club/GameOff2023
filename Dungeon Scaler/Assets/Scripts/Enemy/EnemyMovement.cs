3d612fc61e9b85fc4de263336ab2d33f30d438b1
ssmith817 <salhoss@outlook.com>
2023-11-15 17:59:14 -0600

    Basic Enemy Movement
    Currently in progress:
    Enemy moving in wrong direction

---
+++ Dungeon Scaler/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+
+    private Rigidbody2D rigidBody;
+    private PlayerAwarenessController playerAwarenessController;
+    private Vector2 targetDirection;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerAwarenessController = GetComponent<PlayerAwarenessController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        UpdateTargetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
+    }
+
+    private void UpdateTargetDirection()
+    {
+        if (playerAwarenessController.AwareOfPlayer)
+        {
+            targetDirection = playerAwarenessController.DirectionToPlayer;
+        }
+        else
+        {
+            targetDirection = Vector2.zero;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        if (targetDirection == Vector2.zero)
+        {
+            return;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        rigidBody.SetRotation(rotation);
+    }
+
+    private void SetVelocity()
+    {
+        if (targetDirection == Vector2.zero)
+        {
+            rigidBody.velocity = Vector2.zero;
+        }
+        else
+        {
+            rigidBody.velocity = transform.up * speed;
+        }
+    }
+}
---
+++ Dungeon Scaler/Assets/Scripts/Enemy/PlayerAwarenessController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAwarenessController : MonoBehaviour
+{
+
+    public bool AwareOfPlayer { get; private set; }
+
+    public Vector2 DirectionToPlayer { get; private set; }
+
+    [SerializeField]
+    private float playerAwarenessDistance;
+
+    private Transform player;
+     
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        Vector2 enemyToPlayerVector = player.position - transform.position;
+        DirectionToPlayer = enemyToPlayerVector.normalized;
+
+        if (enemyToPlayerVector.magnitude <= playerAwarenessDistance )
+        {
+            AwareOfPlayer = true;
+        }
+        else
+        {
+            AwareOfPlayer = false;
+        }
+    }
+}
