f108317bbe8b930dada99bc4b4ec1c4565d23b56
Gunnar Dahl <gunnardahl4@gmail.com>
2023-11-20 15:04:05 -0600

    Sorted sprites correctly
    i did it

--- Dungeon Scaler/Assets/Scripts/Tilemap_Scripts/SortSprite.cs
+++ Dungeon Scaler/Assets/Scripts/Tilemap_Scripts/SortSprite.cs
@@ -5,24 +5,35 @@ using UnityEngine.Tilemaps;
 
 public class SortSprite : MonoBehaviour
 {
-    public Vector3 relativePos; //debug
     public Vector3 pos; //debug
-    public int characterZ = 0; //debug
+    public Vector3Int tilePos; //debug
+    public TileBase tileBase; //debug
+    public Vector3Int playerPos; //debug
+    public float newZ; //debug
+    public int spriteZ = 0;
 
     Grid grid;
+    Tilemap tilemap;
 
     void Start() {
         grid = GameObject.FindWithTag("Grid").GetComponent<Grid>();
+        tilemap = GameObject.FindWithTag("Tilemap").GetComponent<Tilemap>();
     }
 
     void Update() {
         pos = new Vector3(transform.position.x, transform.position.y, 0);
-        Vector3Int tilepos = grid.WorldToCell(pos);
-        Vector3 tileworldpos = grid.CellToWorld(tilepos);
-        relativePos = pos - tileworldpos;
-        if (relativePos.y < .25)
-            transform.position = new Vector3(transform.position.x, transform.position.y, 1 + characterZ);
-        else
-            transform.position = new Vector3(transform.position.x, transform.position.y, 2 + characterZ);
+        tilePos = tilemap.WorldToCell(pos);
+
+        for (int i = 0; i < 100; i++) {
+            tileBase = tilemap.GetTile(tilePos);
+            if (tileBase != null) {
+                playerPos = tilePos;
+                playerPos.z++;
+            }
+            tilePos = new Vector3Int(tilePos.x - 1, tilePos.y - 1, tilePos.z + 1);
+        }
+
+        newZ = tilemap.CellToWorld(playerPos).z;
+        transform.position = new Vector3(transform.position.x, transform.position.y, newZ);
     }
 }
